95dbac408d32d868cdc60e3f28b1be9e162532f8
AshishSonu2003 <119962072+AshishSonu2003@users.noreply.github.com>
2023-03-30 19:38:10 +0530

    Add files via upload

--- Train Question.py
+++ Train Question.py
@@ -25,6 +25,11 @@ class LinkedList:
         self.head = None
         self.tail = None
         self.length = 0
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current.data
+            current = current.next
 
     def append(self, data):
         new_node = Node(data)
@@ -58,33 +63,29 @@ class LinkedList:
             leader.next = new_node
             new_node.next = holding_pointer
             self.length += 1
-
-
-
-
 class Train:
     def __init__(self, train_name, compartment_list=None):
-        self.train_name = train_name
-        self.compartment_list = LinkedList()
+        self.__train_name = train_name
+        self.__compartment_list = LinkedList()
 
-        if compartment_list is not None:
-            for compartment in compartment_list:
+        if self.__compartment_list is not None:
+            for compartment in self.__compartment_list:
                 self.add_compartment(compartment)
 
     def add_compartment(self, compartment):
         if isinstance(compartment, Compartment):
-            self.compartment_list.append(compartment)
+            self.__compartment_list.append(compartment)
         else:
             print("Invalid compartment object")
 
     def remove_compartment(self, name):
-        current_node = self.compartment_list.head
+        current_node = self.__compartment_list.head
         prev_node = None
 
         while current_node is not None:
             if current_node.data.get_name() == name:
                 if prev_node is None:
-                    self.compartment_list.head = current_node.next
+                    self.__compartment_list.head = current_node.next
                 else:
                     prev_node.next = current_node.next
                 return True
@@ -92,36 +93,31 @@ class Train:
                 prev_node = current_node
                 current_node = current_node.next
 
-        print(f"Compartment {name} not found")
+        print("Compartment {} not found".format(name))
         return False
 
     def insert_compartment_at_beginning(self, compartment):
         if isinstance(compartment, Compartment):
-            self.compartment_list.prepend(compartment)
+            self.__compartment_list.prepend(compartment)
         else:
             print("Invalid compartment object")
 
     def insert_compartment_at_end(self, compartment):
         if isinstance(compartment, Compartment):
-            self.compartment_list.append(compartment)
+            self.__compartment_list.append(compartment)
         else:
             print("Invalid compartment object")
-
     def insert_compartment_at_position(self, compartment, position):
         if isinstance(compartment, Compartment):
-            self.compartment_list.insert(position, compartment)
+            self.__compartment_list.insert(position, compartment)
         else:
             print("Invalid compartment object")
-
-
- 
-
     def get_train_name(self):
         return self.train_name
 
     def get_compartment_list(self):
         compartments = []
-        current_node = self.compartment_list.head
+        current_node = self.__compartment_list.head
 
         while current_node is not None:
             compartments.append(current_node.data)
@@ -132,16 +128,15 @@ class Train:
 
     def check_vacancy(self):
         count = 0
-        current_node = self.compartment_list.head
+        current_node = self.__compartment_list.head
 
         while current_node is not None:
             if current_node.data.is_vacant():
                 count += 1
             current_node = current_node.next
-
         return count
 train = Train("Express", [])
-compartment1 = Compartment("A1", 50, 30)
+compartment1 = Compartment("Sleeper", 50, 30)
 compartment2 = Compartment("A2", 40, 20)
 train.add_compartment(compartment1)
 train.add_compartment(compartment2)
@@ -150,7 +145,7 @@ print("Compartments with more than 50% vacancy:", num_vacant_compartments)
 train.remove_compartment("A2")
 compartment3 = Compartment("B1", 30, 10)
 train.insert_compartment_at_beginning(compartment3)
-compartment4 = Compartment("C1", 60, 40)
+compartment4 = Compartment("Chair Car", 60, 40)
 train.insert_compartment_at_end(compartment4)
 compartment_list = train.get_compartment_list()
 for compartment in compartment_list:
